67dd278dee9c898eb84b25f223afb844a2deae62
JoeMBH <joembhu@gmail.com>
2023-06-15 22:42:49 +0300

    grid layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -12,7 +13,43 @@ export default function Home() {
       overflow-y-auto
       "
     >
-      <Header>Header</Header>
+      <Header>
+        <div className="mb-2">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            "
+          >
+            Welcome back
+          </h1>
+          <div
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            "
+          >
+            <ListItem
+              name="Liked Songs"
+              image="/images/liked.png"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
+
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>List of Songs</div>
+      </div>
     </div>
   );
 }

c5d4a9278dda88acfaeb70f379e54a69501625da
JoeMBH <joembhu@gmail.com>
2023-06-15 22:10:44 +0300

    navbar buttons

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,8 +1,23 @@
+import { twMerge } from "tailwind-merge";
+
 interface BoxProps {
   children: React.ReactNode;
   className?: string;
 }
 const Box: React.FC<BoxProps> = ({ children, className }) => {
-  return <div>{children}</div>;
+  return (
+    <div
+      className={twMerge(`
+        bg-neutral-900
+        rounded-lg
+        h-fit
+        w-full
+      `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 export default Box;
